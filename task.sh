cb2465958df06c311f87f8cfa54c383ed761e7bf
Yousseftahaa <127946204+Yousseftahaa@users.noreply.github.com>
2025-01-31 11:01:29 +0200

    task.sh

--- task.sh
+++ task.sh
@@ -1,24 +1,40 @@
-System Report 
-started on: Fri Jan 31 10:34:23 AM EET 2025
- Disk Usage 
-Filesystem      Size  Used Avail Use% Mounted on
-udev            1.9G     0  1.9G   0% /dev
-tmpfs           389M  1.4M  387M   1% /run
-/dev/sda1        19G  6.1G   12G  35% /
-tmpfs           1.9G     0  1.9G   0% /dev/shm
-tmpfs           5.0M  8.0K  5.0M   1% /run/lock
-tmpfs           389M   92K  388M   1% /run/user/1000
- CPU Usage 
- Memory Usage 
-               total        used        free      shared  buff/cache   available
-Mem:           3.8Gi       1.2Gi       2.1Gi        15Mi       713Mi       2.6Gi
-Swap:          974Mi          0B       974Mi
- Top 5 Memory-Consuming Processes 
-USER         PID %CPU %MEM    VSZ   RSS TTY      STAT START   TIME COMMAND
-youssef+    1634 10.7  6.5 3744232 259192 ?      Ssl  10:33   0:05 /usr/bin/gnome-shell
-youssef+    1831  5.3  2.1 805476 87020 ?        Sl   10:33   0:02 /usr/bin/gnome-software --gapplication-service
-youssef+    2153  0.8  2.0 548164 81024 ?        Ssl  10:33   0:00 /usr/libexec/gsd-xsettings
-root        1184 33.5  2.0 423116 80076 ?        Ssl  10:33   0:20 /usr/libexec/packagekitd
-youssef+    1836  1.1  1.8 794208 73200 ?        Sl   10:33   0:00 /usr/libexec/evolution-data-server/evolution-alarm-notify
+#!/bin/bash
 
-Report saved to report.log
+
+OUTPUT_FILE="report.log"
+
+THRESHOLD=80
+
+echo " System Report " > "$OUTPUT_FILE"
+echo "started on: $(date)" >> "$OUTPUT_FILE"
+
+
+echo -e " Disk Usage " >> "$OUTPUT_FILE"
+df -h | awk -v threshold=$THRESHOLD '
+NR==1 {print $0} 
+NR>1 {
+    use=$5+0  # 3shan akhly elpercent number bas 
+    if (use > threshold) {
+        print $0 " [WARNING: Usage exceeds " threshold "%]"
+    } else {
+        print $0
+    }
+}' >> "$OUTPUT_FILE"
+
+
+echo -e " CPU Usage " >> "$OUTPUT_FILE"
+top -bn1 | grep "Cpu(s)" >> "$OUTPUT_FILE"
+
+
+echo -e " Memory Usage " >> "$OUTPUT_FILE"
+free -h >> "$OUTPUT_FILE"
+
+
+echo -e " Top 5 Memory-Consuming Processes " >> "$OUTPUT_FILE"
+ps aux --sort=-%mem | head -n 6 >> "$OUTPUT_FILE"
+
+
+cat "$OUTPUT_FILE"
+
+
+echo -e "\nReport saved to $OUTPUT_FILE"
